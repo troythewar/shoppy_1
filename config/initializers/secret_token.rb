c424c095c6eb305830146cecd90833013674581d
deep <deep@deep-VirtualBox.(none)>
2012-12-14 02:40:21 +0530

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppySample::Application.config.secret_token = '2490fc8298033d745ccf81c54c513b125c26ce14fbc98c137916992160117e96033716d4d741e5e9b2a4f472328a0c85776b8771367b21914ea523390be88eca'
